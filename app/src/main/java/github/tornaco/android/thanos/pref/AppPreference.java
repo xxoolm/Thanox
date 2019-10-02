7cedce37f7b96737a927aa0560ca07287dec2e32
tornaco <tornaco@163.com>
2019-10-02 10:58:52 +0800

    Update first run policy
    Signed-off-by: tornaco <tornaco@163.com>

--- app/src/main/java/github/tornaco/android/thanos/pref/AppPreference.java
+++ app/src/main/java/github/tornaco/android/thanos/pref/AppPreference.java
@@ -2,10 +2,11 @@ package github.tornaco.android.thanos.pref;
 
 import android.content.Context;
 import androidx.preference.PreferenceManager;
+import github.tornaco.android.thanos.BuildConfig;
 
 public class AppPreference {
 
-    private static final String PREF_KEY_FIRST_RUN = "PREF_KEY_FIRST_RUN";
+    private static final String PREF_KEY_FIRST_RUN = "PREF_KEY_FIRST_RUN_" + BuildConfig.VERSION_NAME;
 
     public static boolean isFirstRun(Context context) {
         return PreferenceManager.getDefaultSharedPreferences(context)
