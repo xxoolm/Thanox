8082c2fa983dc64c2b4d5c117524e546afc6e899
tornaco <tornaco@163.com>
2019-10-25 21:51:43 +0800

    Fix useless import
    Signed-off-by: tornaco <tornaco@163.com>

--- android_framework/base/src/main/java/github/tornaco/android/thanos/core/app/ActivityManager.java
+++ android_framework/base/src/main/java/github/tornaco/android/thanos/core/app/ActivityManager.java
@@ -3,7 +3,6 @@ package github.tornaco.android.thanos.core.app;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
-import android.os.RemoteException;
 import github.tornaco.android.thanos.core.app.start.StartRecord;
 import github.tornaco.android.thanos.core.process.ProcessRecord;
 import lombok.AllArgsConstructor;
