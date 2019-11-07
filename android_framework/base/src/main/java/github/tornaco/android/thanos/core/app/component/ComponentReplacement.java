9e4137db872ea28f8c2c3890fa16006b99783e07
tornaco <tornaco@163.com>
2019-11-07 23:03:35 +0800

    Fix checkstyle: '类 ComponentReplacement 应定义为 final 的'

--- android_framework/base/src/main/java/github/tornaco/android/thanos/core/app/component/ComponentReplacement.java
+++ android_framework/base/src/main/java/github/tornaco/android/thanos/core/app/component/ComponentReplacement.java
@@ -11,7 +11,7 @@ import lombok.ToString;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
-public class ComponentReplacement implements Parcelable {
+public final class ComponentReplacement implements Parcelable {
 
     public ComponentName from, to;
 
