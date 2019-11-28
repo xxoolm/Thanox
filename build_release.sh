6eb8b9fc416c967da3cd5c5c2d65bc3ebacacdf7
tornaco <tornaco@163.com>
2019-11-28 22:25:06 +0800

    Update build script

--- build_release.sh
+++ build_release.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 java -version
 
-
 rm -rf out
 
 ./gradlew clean
-
-./gradlew :android_framework:base:idlGen --stacktrace
-./gradlew :android_framework:base:build --stacktrace
-
-./gradlew :android_framework:services:build --stacktrace
-
-./gradlew :app:assembleRelease --stacktrace
+./gradlew :module_locker:installPrcRelease
+./gradlew :app:installPrcRelease
