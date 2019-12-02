399c320e2926160b971d5991c9018ee8770829fc
tornaco <tornaco@163.com>
2019-12-02 23:53:08 +0800

    Add removeAccessibilityInteractionConnection hooks

--- android_framework/patchx/src/main/java/github/tornaco/android/thanos/services/xposed/hooks/accessibility/AccessibilityManagerServiceRgistry.java
+++ android_framework/patchx/src/main/java/github/tornaco/android/thanos/services/xposed/hooks/accessibility/AccessibilityManagerServiceRgistry.java
@@ -72,8 +72,8 @@ public class AccessibilityManagerServiceRgistry implements IXposedHook {
                     BootStrap.THANOS_X
                             .getWindowManagerService()
                             .onIAccessibilityInteractionConnectionAttach(
-                                    (IAccessibilityInteractionConnection) param.args[0],
-                                    (IWindow) param.args[1],
+                                    (IAccessibilityInteractionConnection) param.args[1],
+                                    (IWindow) param.args[0],
                                     (int) param.args[2]);
                 }
             });
